ae86c748609ea7a6b6dffde138b614f8813b2e19
smarr2198 <smarr2198@gmail.com>
2022-04-09 13:28:42 -0500

    added file structure

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -6,6 +6,7 @@ const { Student, Course } = require("../models");
 const headCount = async () =>
   Student.aggregate()
     // Your code here
+    .count("studentCount")
     .then((numberOfStudents) => numberOfStudents);
 
 // Execute the aggregate method on the Student model and calculate the overall grade by using the $avg operator
@@ -14,6 +15,7 @@ const grade = async (studentId) =>
     // TODO: Ensure we include only the student who can match the given ObjectId using the $match operator
     {
       // Your code here
+      $match: { _id: ObjectId(studentId) },
     },
     {
       $unwind: "$assignments",
@@ -21,6 +23,10 @@ const grade = async (studentId) =>
     // TODO: Group information for the student with the given ObjectId alongside an overall grade calculated using the $avg operator
     {
       // Your code here
+      $group: {
+        _id: ObjectId(studentId),
+        overallGrade: { $avg: "$assignments.score" },
+      },
     },
   ]);
 
@@ -31,7 +37,7 @@ module.exports = {
       .then(async (students) => {
         const studentObj = {
           students,
-          // headCount: await headCount(),
+          headCount: await headCount(),
         };
         return res.json(studentObj);
       })
